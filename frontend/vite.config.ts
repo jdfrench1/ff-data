e631c6ca5473e19bbfce45e506a53f878e7e9b14
Jordon French <jordon.french@gmail.com>
2025-10-10 22:57:45 -0600

    Replace frontend scaffold with Vue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
